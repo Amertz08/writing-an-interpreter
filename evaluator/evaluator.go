4ef81755d77e56bdee5c6f2e78289e68e4f26b20
Adam <adammertz@gmail.com>
2026-02-21 17:14:53 -0600

    Add support for evaluating integer literals with tests

---
+++ evaluator/evaluator.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"writing-an-intrpreter/ast"
+	"writing-an-intrpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
+}
