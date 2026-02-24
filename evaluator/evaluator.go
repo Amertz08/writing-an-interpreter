cd3ec0c3d7c99553241d5ccb624971638c795eb0
Adam <adammertz@gmail.com>
2026-02-24 07:48:05 -0600

    Add support for closures and nested environments with corresponding helper functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -78,10 +78,37 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if len(args) == 1 && isError(args[0]) {
 			return args[0]
 		}
+		return applyFunction(function, args)
 	default:
 		return NULL
 	}
 	return nil
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if ret, ok := obj.(*object.ReturnValue); ok {
+		return ret.Value
+	}
+	return obj
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
--- object/environment.go
+++ object/environment.go
@@ -1,17 +1,26 @@
 package object
 
+func NewEnclosedEnvironment(parent *Environment) *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: parent}
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 // Environment exists to store values to assigned variables
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
